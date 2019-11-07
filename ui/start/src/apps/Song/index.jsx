79c11a020c22dd69683d9cc58ecf789c7e1c8c13
Jared Selcoe <jared.orion.selcoe@gmail.com>
2019-11-07 00:04:53 -0600

    UI - add Song app

--- ui/start/src/apps/Song/index.jsx
+++ ui/start/src/apps/Song/index.jsx
@@ -1,13 +1,83 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import Typography from '@material-ui/core/Typography'
+
+import Spinner from 'components/Spinner'
+import ErrorMessage from 'components/ErrorMessage'
+
+import { EVERYTHING, getSongErrorCodes } from 'schemaValues'
 
 import './styles.css'
+
+const GET_SONG = gql`
+  query GetSong($id: ID!) {
+    song(id: $id) {
+      success
+      message
+      song {
+        artist
+        name
+        lyrics
+        reason
+        user {
+          id
+          email
+        }
+      }
+    }
+  }
+`
 
 const Song = ({
   match: {
     params: { id },
   },
-}) => <div>Song</div>
+}) => {
+  const { data, loading, error } = useQuery(GET_SONG, { variables: { id } })
+
+  if (loading) return <Spinner />
+  if (error) return <ErrorMessage />
+
+  const {
+    song: { success, message },
+  } = data
+  if (!success) {
+    return message === getSongErrorCodes.NO_SONG ? (
+      <ErrorMessage msg="Uh oh! Looks like that song doesn't exist." />
+    ) : (
+      <ErrorMessage />
+    )
+  }
+
+  const {
+    song: {
+      song: { artist, name, lyrics, reason, user },
+    },
+  } = data
+  return (
+    <>
+      <Typography variant="h2" color="primary" className="w-100 center pt4">
+        {name}
+      </Typography>
+      <Typography variant="h5" color="textPrimary" className="w-100 center">
+        by {artist}
+      </Typography>
+      <Typography
+        variant="subtitle1"
+        color="textSecondary"
+        className="w-100 center"
+        styleName="reason"
+      >
+        {user.email} loves {reason === EVERYTHING ? 'everything about it' : `the ${reason}`}
+      </Typography>
+      <Typography className="pt4" styleName="lyrics">
+        {lyrics || `We couldn't find any lyrics for this song`}
+      </Typography>
+    </>
+  )
+}
 
 Song.propTypes = {
   match: PropTypes.shape({
