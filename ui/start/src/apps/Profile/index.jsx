78d3ac32f2e4459113f3eaa2f611d04e5a4c7e97
Jared Selcoe <jared.orion.selcoe@gmail.com>
2019-11-07 09:29:51 -0600

    UI - add new song entry to Profile app (not submitting yet)

--- ui/start/src/apps/Profile/index.jsx
+++ ui/start/src/apps/Profile/index.jsx
@@ -2,12 +2,18 @@ import React, { useState } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 import gql from 'graphql-tag'
 import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Button from '@material-ui/core/Button'
 
 import { getUser } from 'lib/storage'
 
 import Spinner from 'components/Spinner'
 import ErrorMessage from 'components/ErrorMessage'
 import Listing from 'components/Listing'
+import TextInput from 'components/TextInput'
+import Dropdown from 'components/Dropdown'
+
+import { EVERYTHING, LYRICS, SOUND } from 'schemaValues'
 
 import './styles.css'
 
@@ -30,6 +36,9 @@ const GET_USER = gql`
 const Profile = () => {
   const { id, email } = getUser()
 
+  const [artist, setArtist] = useState('')
+  const [name, setName] = useState('')
+  const [reason, setReason] = useState('')
   const [favorites, setFavorites] = useState([])
 
   const { loading, error } = useQuery(GET_USER, {
@@ -51,6 +60,16 @@ const Profile = () => {
     setFavorites(updatedFavorites)
   }
 
+  const handleArtistChange = ({ target: { value } }) => {
+    setArtist(value)
+  }
+  const handleNameChange = ({ target: { value } }) => {
+    setName(value)
+  }
+  const handleReasonChange = ({ target: { value } }) => {
+    setReason(value)
+  }
+
   if (loading) return <Spinner />
   if (error) return <ErrorMessage />
 
@@ -62,6 +81,50 @@ const Profile = () => {
       <Typography variant="h6" color="textSecondary" className="w-100 center pt2 pb4">
         {email}
       </Typography>
+      <div className="w-100 flex-centered">
+        <Paper styleName="form">
+          <Typography variant="h4" color="secondary" className="center pv3">
+            Add a new favorite
+          </Typography>
+          <div styleName="text">
+            <TextInput
+              name="Song name"
+              label="Song name"
+              value={name}
+              onChange={handleNameChange}
+              fullWidth
+            />
+          </div>
+          <div styleName="text">
+            <TextInput
+              name="Artist name"
+              label="Artist name"
+              value={artist}
+              onChange={handleArtistChange}
+              fullWidth
+            />
+          </div>
+          <div styleName="dropdown">
+            <Dropdown
+              name="Reason"
+              label="Reason"
+              className="pb2 w-100"
+              options={[
+                { value: EVERYTHING, display: 'Everything' },
+                { value: LYRICS, display: 'Lyrics' },
+                { value: SOUND, display: 'Sound' },
+              ]}
+              onChange={handleReasonChange}
+              value={reason}
+            />
+          </div>
+          <div styleName="button">
+            <Button type="submit" variant="contained" color="secondary" fullWidth>
+              Add
+            </Button>
+          </div>
+        </Paper>
+      </div>
       {favorites.map(song => (
         <Listing
           canEdit
