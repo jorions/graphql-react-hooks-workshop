9b219b2def7471f9a730f1e6a7b9196acdc1f80c
Jared Selcoe <jared.orion.selcoe@gmail.com>
2019-11-07 00:24:11 -0600

    UI - add basic Profile app with first query

--- ui/start/src/apps/Profile/index.jsx
+++ ui/start/src/apps/Profile/index.jsx
@@ -1,7 +1,71 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+import Typography from '@material-ui/core/Typography'
+
+import { getUser } from 'lib/storage'
+
+import Spinner from 'components/Spinner'
+import ErrorMessage from 'components/ErrorMessage'
+import Listing from 'components/Listing'
 
 import './styles.css'
 
-const Profile = () => <div>Profile</div>
+const GET_USER = gql`
+  query GetUser($id: ID!) {
+    user(id: $id) {
+      success
+      user {
+        favorites {
+          id
+          artist
+          name
+          reason
+        }
+      }
+    }
+  }
+`
+
+const Profile = () => {
+  const { id, email } = getUser()
+
+  const [favorites, setFavorites] = useState([])
+
+  const { loading, error } = useQuery(GET_USER, {
+    variables: { id },
+    // We want to make sure this page is always as up-to-date as possible, so
+    // don't rely on the default setting 'cache-first'
+    fetchPolicy: 'network-only',
+    onCompleted({ user: { success, user } }) {
+      if (success) setFavorites(user.favorites)
+    },
+  })
+
+  if (loading) return <Spinner />
+  if (error) return <ErrorMessage />
+
+  return (
+    <>
+      <Typography variant="h2" color="primary" className="w-100 center pt4">
+        Your Profile
+      </Typography>
+      <Typography variant="h6" color="textSecondary" className="w-100 center pt2 pb4">
+        {email}
+      </Typography>
+      {favorites.map(song => (
+        <Listing
+          canEdit
+          key={song.id}
+          songId={song.id}
+          artist={song.artist}
+          createdAt={song.createdAt}
+          reason={song.reason}
+          name={song.name}
+        />
+      ))}
+    </>
+  )
+}
 
 export default Profile
